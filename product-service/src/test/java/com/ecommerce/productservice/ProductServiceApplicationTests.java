6473799e582e8c244e58571d14ccad9cd33a8e87
salah <salahdinjemal@gmail.com>
2023-12-29 20:21:46 -0600

    add integration test

--- product-service/src/test/java/com/ecommerce/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/ecommerce/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,45 @@
 package com.ecommerce.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.3");
+
+	@Autowired
+	private MockMvc mockMvc;
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\n" +
+						"    \"name\": \"Product 1\",\n" +
+						"    \"description\": \"Product 1 description\",\n" +
+						"    \"price\": 100\n" +
+						"}")).andExpect(status().isCreated());
 	}
 
 }
